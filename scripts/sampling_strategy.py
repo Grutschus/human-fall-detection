6ca28b67a8e18d79d226cd8603297f201c0b8482
emiresenov <110808673+emiresenov@users.noreply.github.com>
2023-11-24 11:08:41 +0100

    Create sampling_strategy.py

---
+++ scripts/sampling_strategy.py
@@ -0,0 +1,21 @@
+import argparse
+import logging
+import re
+from pathlib import Path
+import pandas as pd
+
+class SamplingStrategy:
+    def __init__(self, inputPath, outputPath, annotationFilePath):
+        self.inputPath = inputPath
+        self.outputPath = outputPath
+        self.annotationFilePath = annotationFilePath
+
+    
+    def stratifiedSampling(self):
+        annotation_file = pd.read_csv(self.annotationFilePath)
+
+        
+
+
+
+
