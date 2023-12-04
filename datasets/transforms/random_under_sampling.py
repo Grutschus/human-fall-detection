daa8d7321d7ed7405ca93d798d846696b654bbf0
Ola Karrar <ozkarrar2@gmail.com>
2023-12-04 17:37:19 +0100

    "random_under_sampling V1"

--- datasets/transforms/random_under_sampling.py
+++ datasets/transforms/random_under_sampling.py
@@ -1,13 +1,16 @@
 from mmaction.registry import TRANSFORMS  # type: ignore
 from mmcv.transforms import BaseTransform  # type: ignore
 
+import pandas as pd
+import numpy as np
 
 @TRANSFORMS.register_module()
 class RandomUnderSampling(BaseTransform):
-    """Randomly drop samples from the given class."""
+    """Randomly drop samples from a given classes."""
 
-    def __init__(self, drop_ratio: float = 0.2) -> None:
-        self.drop_ratio = drop_ratio
+    def __init__(self, drop_ratios: list(float), to_reduce_classes:list(str)) -> None:
+        self.drop_ratio = drop_ratios
+        self.to_reduce_classes=to_reduce_classes
 
     def transform(self, results: dict) -> dict:
         """Perform the `RandomUnderSampling` transformation.
@@ -18,4 +21,15 @@ class RandomUnderSampling(BaseTransform):
         Returns:
             dict: The result dict.
         """
-        raise NotImplementedError
+        #ToDo: exceptions
+        df= pd.DataFrame.from_dict(results)
+
+        for (ratio,reduce_class) in zip(self.drop_ratios,self.to_reduce_classes):
+            class_indices= df.index[df['label']==reduce_class].tolist()
+            if len(class_indices):
+                to_drop_indices=np.random.choice(class_indices,size=np.ceil(ratio* len(class_indices))).astype(int)
+                df=df.drop(to_drop_indices)
+        results= df.to_dict() 
+        return  results
+
+
